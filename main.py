250eea03fe8830845df3d743219869e3bacafe98
alchemist-of-andoris <the.alchemist.of.andoris@gmail.com>
2025-03-20 23:12:30 -0500

    Finished writing report printing within main function in main.py

--- main.py
+++ main.py
@@ -8,11 +8,19 @@ def get_book_text(file_path):
     return book_text
 
 def main():
-    frankenstein = get_book_text("books/frankenstein.txt")
+    book_file = "books/frankenstein.txt"
+    frankenstein = get_book_text(book_file)
     word_count = count_words(frankenstein)
     char_count = count_chars(frankenstein)
     sorted_dict_list = sort_dict(char_count)
-    print(sorted_dict_list)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at " + book_file + "...")
+    print("----------- Word Count ------------")
+    print("Found " + str(word_count) + " total words")
+    print("--------- Character Count ---------")
+    for char_count in sorted_dict_list:
+        print(char_count["char"] + ": " + str(char_count["count"]))
     return None
 
 main()
