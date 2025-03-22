b998a224bc649c3ac0cccfc52061e6d31856ad53
alchemist-of-andoris <the.alchemist.of.andoris@gmail.com>
2025-03-22 10:25:02 -0500

    modified main.py to accept filepath as an arg instead of hardcoding text tiles.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from stats import count_words, count_chars, sort_dict
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as b:
@@ -8,12 +9,16 @@ def get_book_text(file_path):
     return book_text
 
 def main():
-    book_file = "books/frankenstein.txt"
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_file = sys.argv[1]
     frankenstein = get_book_text(book_file)
     word_count = count_words(frankenstein)
     char_count = count_chars(frankenstein)
     sorted_dict_list = sort_dict(char_count)
-
     print("============ BOOKBOT ============")
     print("Analyzing book found at " + book_file + "...")
     print("----------- Word Count ------------")
